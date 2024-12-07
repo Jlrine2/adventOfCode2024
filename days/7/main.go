62ead3934a29ab2e4b3c5ab427eaddc48ec85258
James Rine <rinejames@gmail.com>
2024-12-07 16:39:23 -0500

    Day7

--- days/7/main.go
+++ days/7/main.go
@@ -2,16 +2,60 @@ package main
 
 import (
 	"adventOfCode/common"
+	"strconv"
 )
 
 const day = 7
 
+func canMakeTarget(target int, nums []int) bool {
+	if len(nums) == 2 {
+		res := nums[0]+nums[1] == target || nums[0]*nums[1] == target
+
+		return res
+	}
+	return canMakeTarget(target, append([]int{nums[0] + nums[1]}, nums[2:]...)) || canMakeTarget(target, append([]int{nums[0] * nums[1]}, nums[2:]...))
+}
+
 func Part1(input string) int {
-	return 0
+	lines := common.ToLines(input)
+	s := 0
+	for _, line := range lines {
+		nums := common.Ints(line)
+		target := nums[0]
+		if canMakeTarget(target, nums[1:]) {
+			s += target
+		}
+	}
+	return s
+}
+func canMakeTargetWithConcat(target int, nums []int) bool {
+	first := strconv.Itoa(nums[0])
+	second := strconv.Itoa(nums[1])
+
+	sum := nums[0] + nums[1]
+	prod := nums[0] * nums[1]
+	concat, _ := strconv.Atoi(first + second)
+	if len(nums) == 2 {
+		res := sum == target || prod == target || concat == target
+		return res
+	}
+	return canMakeTargetWithConcat(target, append([]int{sum}, nums[2:]...)) ||
+		canMakeTargetWithConcat(target, append([]int{prod}, nums[2:]...)) ||
+		canMakeTargetWithConcat(target, append([]int{concat}, nums[2:]...))
 }
 
 func Part2(input string) int {
-	return 1
+	lines := common.ToLines(input)
+	s := 0
+	for _, line := range lines {
+		nums := common.Ints(line)
+		target := nums[0]
+		b := canMakeTargetWithConcat(target, nums[1:])
+		if b {
+			s += target
+		}
+	}
+	return s
 }
 
 func main() {
